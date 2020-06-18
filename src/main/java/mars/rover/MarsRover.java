e69c5f4307d0f7a36a24070c6a8ba61ac2e744dc
Samuel Gerhartinger <samuel.g.anmeldung@gmail.com>
2020-06-18 18:29:23 +0200

    Finish

--- src/main/java/mars/rover/MarsRover.java
+++ src/main/java/mars/rover/MarsRover.java
@@ -10,29 +10,32 @@ public class MarsRover {
     public static String move(int x, int y, char direction, String instructions) {
         if (!instructions.isEmpty()) {
 
-            position = x+" "+y+" "+direction;
+            if (position.isEmpty()){
+                position = x+" "+y+" "+direction;
+            }
+            int[] go = {1,1,-1,-1};
 
             char instruction = instructions.charAt(0);
             if(instruction == 'L'){
-                return move(x,y, heaven[(Arrays.asList(heaven).indexOf(direction)+4 -1 )%4], instructions.substring(1, instructions.length()));
+                return move(x,y, heaven[(Arrays.asList(heaven).indexOf(direction)+4 -1 )%4], instructions.substring(1));
             }else if(instruction == 'R'){
-                return move(x,y, heaven[(Arrays.asList(heaven).indexOf(direction) + 1 )%4], instructions.substring(1, instructions.length()));
+                return move(x,y, heaven[(Arrays.asList(heaven).indexOf(direction) + 1 )%4], instructions.substring(1));
             }else if(instruction == 'M'){
-
-            }
-
-            if (instruction == 'M') {
-                if (direction == 'N') {
-                    return move(x, y + 1, 'N', instructions.substring(1, instructions.length()));
-                } else if (direction == 'S') {
-                    return move(x, y - 1, 'S', instructions.substring(1, instructions.length()));
-                } else if (direction == 'W') {
-                    return move(x - 1, y, 'W', instructions.substring(1, instructions.length()));
-                } else if (direction == 'E') {
-                    return move(x + 1, y, 'E', instructions.substring(1, instructions.length()));
+                int i = Arrays.asList(heaven).indexOf(direction);
+                if (i % 2 == 0){
+                    y+= go[i];
+                }else {
+                    x+= go[i];
                 }
+                if(x < 0 || y < 0){
+                    String[] pos = position.split(" ");
+                    position="";
+                    return move(Integer.parseInt(pos[0]), Integer.parseInt(pos[1]),pos[2].charAt(0),"");
+                }
+                return move(x,y, direction, instructions.substring(1));
             }
         }
+        position="";
         return x + " " + y + " " + direction;
     }
 }
